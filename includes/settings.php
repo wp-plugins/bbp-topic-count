0648c4adb33e0e0a596c2015329901eadf5cb5e9
Robin W <Robin W@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-03 08:44:35 +0000

    updated for 3.9
    git-svn-id: https://plugins.svn.wordpress.org/bbp-topic-count/trunk@907291 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/settings.php
+++ includes/settings.php
@@ -27,7 +27,7 @@ function tc_settings_page()
 		<td>
 			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="9NKN2K8SXGS9Q">
+<input type="hidden" name="hosted_button_id" value="S6PZGWPG3HLEA">
 <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
 </form>
